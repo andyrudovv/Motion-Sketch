63e8023af8d806320126073cd7b935a6eafc7db2
andyrudovv <110022906+andyrudovv@users.noreply.github.com>
2024-12-20 01:19:19 +0000

    &&>

--- src/main.rs
+++ src/main.rs
@@ -43,15 +43,15 @@ impl ApplicationHandler for App {
 fn main() {
     let event_loop = EventLoop::new().unwrap();
 
-// ControlFlow::Poll continuously runs the event loop, even if the OS hasn't
-// dispatched any events. This is ideal for games and similar applications.
-event_loop.set_control_flow(ControlFlow::Poll);
+    // ControlFlow::Poll continuously runs the event loop, even if the OS hasn't
+    // dispatched any events. This is ideal for games and similar applications.
+    event_loop.set_control_flow(ControlFlow::Poll);
 
-// ControlFlow::Wait pauses the event loop if no events are available to process.
-// This is ideal for non-game applications that only update in response to user
-// input, and uses significantly less power/CPU time than ControlFlow::Poll.
-event_loop.set_control_flow(ControlFlow::Wait);
+    // ControlFlow::Wait pauses the event loop if no events are available to process.
+    // This is ideal for non-game applications that only update in response to user
+    // input, and uses significantly less power/CPU time than ControlFlow::Poll.
+    event_loop.set_control_flow(ControlFlow::Wait);
 
-let mut app = App::default();
-event_loop.run_app(&mut app);
+    let mut app = App::default();
+    event_loop.run_app(&mut app);
 }
